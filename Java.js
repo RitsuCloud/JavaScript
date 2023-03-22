c61498e3b3aaab1744373ecff262e269e59b5154
RitsuCloud <chonhinchou@gmail.com>
2023-03-22 03:32:41 -0700

    A lot of similarity with Java

---
+++ Java.js
@@ -0,0 +1,23 @@
+/*
+This is the same as java
+*/
+console.log("Hello from javaScript?");  // in-line comments, at the end of a line
+var x = 120; // var can be anything, we can have it as int then as string later
+console.log(x);
+x = "120 string";
+console.log(x);
+
+/*
+Seven data types for JS:
+undefined, null, boolean, string, symbol, number, and object
+*/
+
+// var let us use this varible globally
+var myName = "Beau";
+
+// let will only let us use this within our scope
+let ourName = "ourName";
+
+// var but can't be changed, like final in java
+const pi = 3.14;
+
